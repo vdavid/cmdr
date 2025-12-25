91d4d787e25010ec79b2eb3f8670f7cde1ee2d41
David Veszelovszki <veszelovszki@gmail.com>
2025-12-25 18:37:15 +0100

    Add Tailwind 4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-    plugins: [sveltekit()],
+    plugins: [tailwindcss(), sveltekit()],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
@@ -18,10 +19,10 @@ export default defineConfig(async () => ({
         host: host || false,
         hmr: host
             ? {
-                  protocol: 'ws',
-                  host,
-                  port: 1421,
-              }
+                protocol: 'ws',
+                host,
+                port: 1421,
+            }
             : undefined,
         watch: {
             // 3. tell Vite to ignore watching `src-tauri`
