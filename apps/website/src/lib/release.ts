aee31b2c59b68585405bed6f053e3459355b64a2
David Veszelovszki <veszelovszki@gmail.com>
2026-01-14 00:47:32 +0100

    Fix release process plus manual latest.json update

--- apps/website/src/lib/release.ts
+++ apps/website/src/lib/release.ts
@@ -1,4 +1,4 @@
 import latestRelease from '../../public/latest.json'
 
 export const version = latestRelease.version
-export const dmgUrl = `https://github.com/vdavid/cmdr/releases/download/v${version}/Cmdr_universal.dmg`
+export const dmgUrl = `https://github.com/vdavid/cmdr/releases/download/v${version}/Cmdr_${version}_universal.dmg`
